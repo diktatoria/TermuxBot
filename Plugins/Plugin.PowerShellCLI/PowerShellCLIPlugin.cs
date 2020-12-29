7a2a447ed13f4e09e539f28709a788f5d7e9847e
Johannes <jpinzer@live.de>
2020-12-29 14:08:42 +0100

    Bugfixes

--- Plugins/Plugin.PowerShellCLI/PowerShellCLIPlugin.cs
+++ Plugins/Plugin.PowerShellCLI/PowerShellCLIPlugin.cs
@@ -36,7 +36,7 @@ namespace Plugin.PowerShellCLI
             string scriptData = await File.ReadAllTextAsync("./TermuxInitializeScript.ps1", cancellationToken);
             Task task = this.RunScript(scriptData);
 
-            while (!cancellationToken.IsCancellationRequested) 6
+            while (!cancellationToken.IsCancellationRequested)
             {
                 await Task.Delay(1000, cancellationToken);
             }
