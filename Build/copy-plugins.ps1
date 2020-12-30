6624b2cb83edea7a19b8b8beaee76578491619cb
Johannes <jpinzer@live.de>
2020-12-30 10:52:36 +0100

    Moved Postbuild to Powershell script

---
+++ Build/copy-plugins.ps1
@@ -0,0 +1,14 @@
+param ($solutionDir, $configurationName, $targetName, $targetPath)
+
+$pluginDir = "$solutionDirbin\$configurationName\net5.0\Plugins"
+
+write "Running on $env:OS ..."
+write "Copy Plugins to App directory '$pluginDir' ..."
+
+rm -Recurse "$pluginDir"
+mkdir "$pluginDir"
+Copy $targetPath "$solutionDirbin\$configurationName\net5.0\Plugins\$targetName.dll"
+Copy $targetPath "$solutionDirbin\$configurationName\net5.0\Plugins\$targetName.pdb"
+Copy $targetPath "$solutionDirbin\$configurationName\net5.0\Plugins\$targetName.deps.json"
+
+write "Copy sucessful."
